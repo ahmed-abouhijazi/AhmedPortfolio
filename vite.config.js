400abdaccc841cf18eb576947c0eb4c9871db30c
ahmed-abouhijazi <sabir.ahmed2402@gmail.com>
2023-08-20 06:48:42 -0700

    first commit

--- vite.config.js
+++ vite.config.js
@@ -1,8 +1,19 @@
-import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react'
+// vite.config.js
+import { defineConfig } from 'vite';
+import react from '@vitejs/plugin-react';
 
-// https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
+  build: {
+    rollupOptions: {
+      output: {
+        manualChunks(id) {
+          if (id.includes('node_modules')) {
+            return 'vendor';
+          }
+        },
+      },
+    },
+  },
   base: '/AhmedPortfolio/'
-})
+});
