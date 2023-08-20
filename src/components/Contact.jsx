ed4597504a0dc6eac84d9ae4ed8f8d95737fa83d
ahmed-abouhijazi <sabir.ahmed2402@gmail.com>
2023-08-20 07:16:08 -0700

    first commit

--- src/components/Contact.jsx
+++ src/components/Contact.jsx
@@ -29,16 +29,16 @@ const Contact = () => {
     //click on create a new template then click on save.
     emailjs
       .send(
-        'serviceID', // paste your ServiceID here (you'll get one when your service is created).
-        'templateID', // paste your TemplateID here (you'll find it under email templates).
+        'service_oe5fy1r', // paste your ServiceID here (you'll get one when your service is created).
+        'template_4dtk13p', // paste your TemplateID here (you'll find it under email templates).
         {
           from_name: form.name,
-          to_name: 'YourName', // put your name here.
+          to_name: 'Ahmed ABOUHIJAZI', // put your name here.
           from_email: form.email,
-          to_email: 'youremail@gmail.com', //put your email here.
+          to_email: 'sabir.ahmed2402@gmail.com', //put your email here.
           message: form.message,
         },
-        'yourpublickey' //paste your Public Key here. You'll get it in your profile section.
+        '2y-iNIqGlfrxLxHFy' //paste your Public Key here. You'll get it in your profile section.
       )
       .then(
         () => {
